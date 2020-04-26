e301afee16706826d38ad88cf8c8dab37c699451
Daniyar Maminov <daniyarmaminov@gmail.com>
2020-04-26 18:35:32 +0900

    Add core and main files

---
+++ core_jarvis.py
@@ -0,0 +1,14 @@
+class CoreJarvis:
+    """ Core of the Jarvis helper  """
+
+    def __init__(self):
+        self.duties = {
+            'Living room': 'Nazarbek',
+            'Kitchen top': 'Daniyar',
+            'Kitchen bottom': 'Tima',
+            'Toilets': 'Alisher',
+            'Balcony': 'Sanzhar'}
+
+    def get_duties(self):
+        return self.duties
+
--- main_jarvis.py
+++ main_jarvis.py
@@ -1,6 +1,10 @@
+import json
 import logging
 import os
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from core_jarvis import CoreJarvis
+
+jarvis = CoreJarvis()
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -8,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-TOKEN = ''
+TOKEN = '1256967228:AAEvXSWA5ZEzcqzZi7hWWM3BVe6rLaqvFYg'
 
 PORT = int(os.environ.get('PORT', '8443'))
 
@@ -23,6 +27,11 @@ def echo(update, context):
     update.message.reply_text(update.message.text)
 
 
+def get_duties(update, context):
+    duties = json.dumps(jarvis.get_duties(), indent=2)
+    update.message.reply_text(duties)
+
+
 def main():
     updater = Updater(TOKEN, use_context=True)
 
@@ -31,20 +40,21 @@ def main():
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("duties", get_duties))
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
 
     # Start the Bot
     """ Polling for testing purposes """
-    updater.start_polling()
+#    updater.start_polling()
  
     """ Webhook for production """
-    """ updater.start_webhook(listen="0.0.0.0",
+    updater.start_webhook(listen="0.0.0.0",
                           port=PORT,
                           url_path=TOKEN)
     updater.bot.set_webhook("https://dumb-jarvis.herokuapp.com/" + TOKEN)
-    updater.idle() """
+    updater.idle() 
 
 
 if __name__ == '__main__':
